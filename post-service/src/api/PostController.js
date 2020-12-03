62cef339b7931029400059358865d4ad4541c09c
sidirgot <anastogr@yahoo.com>
2020-12-03 14:25:32 +0200

    Refactored the model and the controller on the post-service since tits going to only one model/controller

--- post-service/src/api/PostController.js
+++ post-service/src/api/PostController.js
@@ -1,11 +1,11 @@
-import Post from "#root/models/Post";
+import PostModel from "./PostModel";
 
 class PostController {
   /**
    * Fetch all posts.
    */
   static async index() {
-    const results = await Post.findAll();
+    const results = await PostModel.findAll();
 
     return results;
   }
--- post-service/src/api/Posts.js
+++ post-service/src/api/Posts.js
@@ -1,5 +1,5 @@
 import express from "express";
-import PostController from "#root/controllers/PostController";
+import PostController from "./PostController";
 
 const router = express.Router();
 
